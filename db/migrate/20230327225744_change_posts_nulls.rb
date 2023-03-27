9925ee44d7e072cd5ae3549ed4499aac543ba76e
Robert Neal <robertwneal2@gmail.com>
2023-03-27 15:58:59 -0700

    Add null: false constraint to Post title and content

---
+++ db/migrate/20230327225744_change_posts_nulls.rb
@@ -0,0 +1,6 @@
+class ChangePostsNulls < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :posts, :title, false
+    change_column_null :posts, :content, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_223149) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_225744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,8 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_223149) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", default: 1
