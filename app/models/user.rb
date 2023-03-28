c83961ee81b0ec0c9c83536d0df99f6c9cde45a2
Robert Neal <robertwneal2@gmail.com>
2023-03-27 19:29:55 -0700

    Add Friendship model and associations to User to get all friends. Can probably be optimized to remove one database query

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,18 @@ class User < ApplicationRecord
   has_many :comments
 
   has_many :friend_invitations, class_name: "FriendRequest", foreign_key: :requester_id
-
   has_many :requested_friends, through: :friend_invitations, source: :recipient
 
   has_many :friend_requests, class_name: "FriendRequest", foreign_key: :recipient_id
-
   has_many :requesting_friends, through: :friend_requests, source: :requester
 
-  has_many :friendships,
-  ->(user) { unscope(:where).where('friend_a_id = ? OR friend_b_id = ?', user.id, user.id) }
+  has_many :a_friendships, class_name: "Friendship", foreign_key: :friend_a_id
+  has_many :a_friends, through: :a_friendships, source: :friend_b
+
+  has_many :b_friendships, class_name: "Friendship", foreign_key: :friend_b_id
+  has_many :b_friends, through: :b_friendships, source: :friend_a
+
+  def friends #queries database twice but is easy to read
+    a_friends + b_friends
+  end
 end
